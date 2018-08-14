c8cdbd06cf57aa8b337ac620a1251454f9d4e119
Joshua Lim <joshualim@Joshuas-Air.fios-router.home>
2018-08-13 21:15:26 -0400

    Image

--- scripts/index.js
+++ scripts/index.js
@@ -32,7 +32,8 @@ $("#seed").val(Math.floor(Math.random() * 1000));
 fire();
 
 function fire() {
-	document.getElementById("cardtop").innerHTML = cardCategories[1];
+	document.getElementById("cardtop").innerHTML = cardCategories[0];
+	document.getElementById("cardsym").innerHTML = "<img src=\"data/" + cardSyms[0] + ".png\" />";
 
 	//get seed and set the seed for randomizer
 	/*var seed = document.getElementById("seed").value;
